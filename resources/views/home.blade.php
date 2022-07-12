cbfc6a50d3d0baa8182c35e2a7751b59ebfbc2a1
Kardast <dottorsf@gmail.com>
2022-07-12 17:11:13 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('templates.base')
+
+@section('pageTitle', 'DC Comics')
+
+@section('pageMain')
+
+    <main>
+        <ul>
+            {{-- @foreach ($arrPaste as $pasta)
+                <li>{{ $pasta['titolo'] }}</li>
+            @endforeach --}}
+            <li>
+                deh
+            </li>
+        </ul>
+    </main>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,25 @@
+<header>
+
+    <div class="navbar">
+
+        <div class="container">
+
+            <div class="logo">
+
+                <img src="{{ asset('img/dc-logo.png') }}" alt="dc-logo">
+            </div>
+            <ul class="navlist">
+                {{-- <li v-for="(link, index) in links" :key="index">
+                <a :class="link.current ? 'here' : ''" href="link.url">{{link.text}}</a>
+                </li> --}}
+
+                @foreach ($arrLinks as $link)
+                    <li>
+                        <a href="{{ route($link['url']) }}">{{ $link['text'] }}</a>
+                    </li>
+                @endforeach
+
+            </ul>
+        </div>
+      </div>
+</header>
