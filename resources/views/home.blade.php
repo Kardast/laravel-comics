fdd40206250bde8e99be9252c6e71a30f9bd9d62
Kardast <dottorsf@gmail.com>
2022-07-12 19:15:23 +0200

    Main Content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,34 @@
 @section('pageMain')
 
     <main>
-        <ul>
-            {{-- @foreach ($arrPaste as $pasta)
-                <li>{{ $pasta['titolo'] }}</li>
-            @endforeach --}}
-            <li>
-                deh
-            </li>
-        </ul>
+        <div class="content">
+
+            <div class="container">
+
+              <div class="series">
+
+                <h2>CURRENT SERIES</h2>
+              </div>
+
+              <div class="comics-container">
+
+                <!-- qui ci va il tag che ti crei nel componente -->
+                {{-- <ComicsCard v-for="(cardiesimo, index) in cards" :key="index" :details="cardiesimo" /> --}}
+                @foreach ($arrComics as $card)
+
+                    <div class="comics-card">
+
+                        <img src="{{ $card['thumb'] }}" alt="{{ $card['series'] }}">
+                        <h4>{{ $card['series'] }}</h4>
+                    </div>
+                @endforeach
+              </div>
+
+              <div class="load-b">
+
+                <a href="#">LOAD MORE</a>
+              </div>
+            </div>
+          </div>
     </main>
 @endsection
